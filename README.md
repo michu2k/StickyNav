f3b0e1d56bedb32592adf6b62baf871e9242560f
michu2k <michalo2k@op.pl>
2019-01-05 19:53:03 +0100

    Published script

---
+++ README.md
@@ -0,0 +1,73 @@
+# StickyNav
+Make the navbar sticky when scrolling the page <br>
+Browsers support: All modern browsers, Internet Explorer 10+
+
+## Version
+1.0.0
+
+## Installation
+
+###### npm
+```
+npm install stickynav-js
+```
+
+###### Github
+You can also download files from github.
+
+## Usage
+On production use JS files only from **dist/** folder
+
+###### 1. Include script
+```html
+<script src="stickyNav.min.js"></script>  
+```
+
+###### 2. Add sticky class in your CSS
+You can change your class name by changing `stickyClass` option 
+
+```css
+.your-navbar.is-sticky {
+    top: 0;
+    z-index: 10;
+    position: fixed;
+    /*...*/
+}
+```
+
+###### 3. Initialize the module
+
+```javascript
+<script>
+    var menu = document.getElementById('navbar');
+    menu.stickyNav();
+</script>
+```
+
+## API
+
+###### Example
+yourElement.stickyNav(options)
+
+* options - object (optional), script options
+
+```javascript
+<script>
+    // Get your element
+    var menu = document.getElementById('navbar');
+    
+    // Pass options
+    menu.stickyNav({
+        stickyClass: 'is-sticky'
+    });
+</script>
+```
+
+###### Options
+
+| Option  | Type | Default value | Description |
+| ----- | ----- | ----- | ----- |
+| stickyClass | string | 'is-sticky' | Sticky class |
+| customBreakPoint | boolean | false | Enable custom breakpoint for navbar |
+| breakPointValue | number | 200 | Navbar breakpoint value in pixels |
+| extraOffset | number | 0 | Add extra offset from the top |
---
+++ src/stickyNav.js
@@ -0,0 +1,135 @@
+/*!
+ * StickyNav v1.0.0
+ * Make the navbar sticky when scrolling the page
+ * https://github.com/michu2k/Accordion
+ *
+ * Copyright 2019 Michał Strumpf
+ * Published under MIT License
+ */
+(function() {
+
+    'use strict';
+
+    /**
+     * Core
+     * @param {object} properties = options defined by user
+     */
+    Object.prototype.stickyNav = function(properties) {
+        const element = this;
+
+        // Default options
+        const defaults = {
+            stickyClass: 'is-sticky', // {string} sticky class
+            customBreakPoint: false, // {boolean} enable custom breakpoint for navbar
+            breakPointValue: 200, // {number} navbar breakpoint value in pixels
+            extraOffset: 0 // {number} add extra offset from the top
+        };
+
+        if (!element.classList) {
+            throw new Error('Sorry, looks like your browser is too old for this script :(');
+        }
+
+        // Extend options 
+        let options = extendDefaults(defaults, properties);
+        // Create extra hidden element
+        let hiddenEl = createHiddenElement();
+
+        let edge = getOffset();
+        let applied = 0;
+
+        // On scroll
+        // Toogle sticky class
+        window.addEventListener('scroll', () => {
+            if (!applied) {
+                if (window.pageYOffset >= edge) {
+                    addSticky();
+                }
+            } else {
+                if (window.pageYOffset <= edge) {
+                    removeSticky();
+                }
+            }
+        });
+
+        // On resize
+        // Update offset
+        window.addEventListener('resize', () => {
+            removeSticky();
+            edge = getOffset();
+
+            if (window.pageYOffset >= edge) {
+                addSticky();
+            }
+        });
+
+        /**
+         * Add sticky class to the element
+         */
+        function addSticky() {
+            hiddenEl.style.height = `${element.offsetHeight}px`;
+            hiddenEl.style.display = 'block';
+            element.classList.add(options.stickyClass);
+            applied = 1;
+        }
+
+        /**
+         * Remove sticky class from the element
+         */
+        function removeSticky() {
+            hiddenEl.style.display = 'none';
+            element.classList.remove(options.stickyClass);
+            applied = 0;
+        }
+
+        /**
+         * Get the offset top value of the element relative to the document
+         * @return {number} offset = offset value
+         */
+        function getOffset() {
+            let offset = element.offsetTop;
+
+            // Add extra offset
+            if (options.extraOffset !== 0) {
+                offset = offset + options.extraOffset;
+            }
+
+            // Custom breakpoint
+            if (options.customBreakPoint) {
+                offset = options.breakPointValue;
+            }
+
+            return offset;
+        }
+
+        /**
+         * Create and add a hidden element to the DOM
+         * @return {object} hidden = created hidden div
+         */
+        function createHiddenElement() {
+            let hidden = document.createElement('div');
+            hidden.style.display = 'none';
+            element.parentNode.insertBefore(hidden, element);
+
+            return hidden;
+        }
+
+        /** 
+         * Extend defaults
+         * @param {object} defaults = defaults options defined in script
+         * @param {object} properties = options defined by user
+         * @return {object} defaults = modified options
+         */
+        function extendDefaults(defaults, properties) {
+           if (properties != null && properties !== undefined) {
+               for (let property in properties) {
+                   defaults[property] = properties[property];
+               }
+           }
+
+           return defaults; 
+        }
+    };
+
+    window.stickyNav = stickyNav;
+
+})();
